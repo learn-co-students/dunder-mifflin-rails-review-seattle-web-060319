2a199c99eb04cfa973d74c94d1ef86272fbd2535
Jason Leach <jlspursfan@gmail.com>
2019-07-02 15:14:31 -0700

    completed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+  validates :title, :alias, {presence: true, uniqueness: true}
+  belongs_to :dog
+
+  def dog_name
+    dog = Dog.find(self.dog_id)
+    dog.name
+  end
 end
