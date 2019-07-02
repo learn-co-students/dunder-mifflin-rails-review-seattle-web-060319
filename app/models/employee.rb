bc930bd1c82fe100617937a78e34446e0d902f82
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-02 14:17:22 -0700

    dunder mifflin lab

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :alias, {presence: true, uniqueness: true}
+    validates :title, {presence: true, uniqueness: true}
+    validates :office, presence: true 
+    validates :dog_id, presence:true
+
 end
