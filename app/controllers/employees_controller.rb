ebe4f85eeb0cc56da714a18a7d566bb383e04adf
Philip Sterling <flatironschool@Flatirons-MacBook-Air-5.local>
2019-07-02 15:25:44 -0700

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,25 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+    def create
+        @dogs = Dog.all
+        @employee= Employee.new(employee_params)
+        if @employee.save
+            redirect_to @employee
+        else
+            render :new
+        end
+    end
+    private
+    def employee_params
+        params.require(:employee).permit(:first_name,:last_name,:title,:office,:alias,:dog_id)
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,13 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+    def self.employees_for_this_dog(dog_object)
+        self.all.select do |employee|
+            employee.dog_id == dog_object.id
+        end
+    end
 end
